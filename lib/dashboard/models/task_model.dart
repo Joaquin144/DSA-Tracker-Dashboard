f1324d3c10969f39f640a2c596c711d04f926f50
vibhu <vibhu.pandey@cropin.com>
2024-09-26 15:55:23 +0530

    integrate firebase

--- lib/dashboard/models/task_model.dart
+++ lib/dashboard/models/task_model.dart
@@ -1,15 +1,17 @@
 class TaskModel {
   final int id;
   final String name;
-  final List<String> tags;
-  final String approach;
-  final double rating;
+  final List<String>? tags;
+  final String? approach;
+  final double? rating;
+  final String url;
 
   TaskModel({
     required this.id,
     required this.name,
-    required this.tags,
-    required this.approach,
-    required this.rating,
+    this.tags,
+    this.approach,
+    this.rating,
+    required this.url,
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:dsa_tracker/app/task_tracker_app.dart';
+import 'package:dsa_tracker/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(TaskTrackerApp());
 }
